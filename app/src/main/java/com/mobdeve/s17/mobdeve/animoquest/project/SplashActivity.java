ffa045380889679f6ed8bcd17995e87761552d75
rnzbobi <rhenzbhovieviloria@gmail.com>
2024-10-07 21:28:03 +0800

    SPLASH SCREEN Developing
    Developed a Splash Screen without MVVM architecture

---
+++ app/src/main/java/com/mobdeve/s17/mobdeve/animoquest/project/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.mobdeve.s17.mobdeve.animoquest.project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_SCREEN_DELAY = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, SPLASH_SCREEN_DELAY);
+    }
+}
