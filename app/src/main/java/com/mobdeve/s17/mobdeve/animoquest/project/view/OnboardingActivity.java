1db9c892310102b654534d66bfc7501f791df888
stvnu <stevenyuuuu@gmail.com>
2024-10-08 19:35:41 +0800

    LOGIN/REGISTER DONE
    - login done
    - register done
    - forgot password done

---
+++ app/src/main/java/com/mobdeve/s17/mobdeve/animoquest/project/view/OnboardingActivity.java
@@ -0,0 +1,49 @@
+package com.mobdeve.s17.mobdeve.animoquest.project.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.mobdeve.s17.mobdeve.animoquest.project.R;
+
+public class OnboardingActivity extends AppCompatActivity {
+
+    Button loginButton;
+    TextView createAccount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_onboarding);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        loginButton = findViewById(R.id.loginButton);
+        createAccount = findViewById(R.id.createAccount);
+    }
+
+    public void loginFunction(View v) {
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void registerFunction (View v) {
+        Intent intent = new Intent(getApplicationContext(), RegistrationActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+}
