743cfe5c9d952a58f025a8528e585700f8a3dd07
rnzbobi <rhenzbhovieviloria@gmail.com>
2024-10-09 01:21:22 +0800

    Integrate Google Map Done
    Added necessary activities for other members, no search view yet on top of Google Map

---
+++ app/src/main/java/com/mobdeve/s17/mobdeve/animoquest/project/view/ElevatorActivity.java
@@ -0,0 +1,88 @@
+package com.mobdeve.s17.mobdeve.animoquest.project.view;
+
+import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.mobdeve.s17.mobdeve.animoquest.project.R;
+
+public class ElevatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_elevator);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Set the indoor_icon to green
+        ImageView elevatorIcon = findViewById(R.id.elevator_icon);
+        elevatorIcon.setColorFilter(getResources().getColor(R.color.green), PorterDuff.Mode.SRC_IN);
+
+        // Set up click listener for indoor_icon
+        ImageView mapIcon = findViewById(R.id.map_icon);
+        mapIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to IndoorNavigationActivity
+                Intent intent = new Intent(ElevatorActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Set up click listener for elevator_icon
+        ImageView indoorIcon = findViewById(R.id.indoor_icon);
+        indoorIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to ElevatorActivity
+                Intent intent = new Intent(ElevatorActivity.this, IndoorNavigationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Set up click listener for elevator_icon
+        ImageView notificationIcon = findViewById(R.id.notification_icon);
+        notificationIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to ElevatorActivity
+                Intent intent = new Intent(ElevatorActivity.this, NotificationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Set up click listener for elevator_icon
+        ImageView profileIcon = findViewById(R.id.profile_icon);
+        profileIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to ElevatorActivity
+                Intent intent = new Intent(ElevatorActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Start the LoginActivity when back button is pressed
+        super.onBackPressed();
+        Intent intent = new Intent(ElevatorActivity.this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish(); // Optional: Call finish to remove this activity from the stack
+    }
+}
--- app/src/main/java/com/mobdeve/s17/mobdeve/animoquest/project/view/MainActivity.java
+++ app/src/main/java/com/mobdeve/s17/mobdeve/animoquest/project/view/MainActivity.java
@@ -1,16 +1,32 @@
 package com.mobdeve.s17.mobdeve.animoquest.project.view;
 
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View; // Import this for View.OnClickListener
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.mobdeve.s17.mobdeve.animoquest.project.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private static final String TAG = "MainActivity";
+    private GoogleMap gMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +38,94 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ImageView mapIcon = findViewById(R.id.map_icon);
+        mapIcon.setColorFilter(getResources().getColor(R.color.green), PorterDuff.Mode.SRC_IN);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.id_map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        } else {
+            Log.e(TAG, "Error - Map Fragment was null!");
+        }
+
+        // Set up click listener for indoor_icon
+        ImageView indoorIcon = findViewById(R.id.indoor_icon);
+        indoorIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to IndoorNavigationActivity
+                Intent intent = new Intent(MainActivity.this, IndoorNavigationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Set up click listener for elevator_icon
+        ImageView elevatorIcon = findViewById(R.id.elevator_icon);
+        elevatorIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to ElevatorActivity
+                Intent intent = new Intent(MainActivity.this, ElevatorActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Set up click listener for elevator_icon
+        ImageView notificationIcon = findViewById(R.id.notification_icon);
+        notificationIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to ElevatorActivity
+                Intent intent = new Intent(MainActivity.this, NotificationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Set up click listener for elevator_icon
+        ImageView profileIcon = findViewById(R.id.profile_icon);
+        profileIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to ElevatorActivity
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        gMap = googleMap;
+
+        try {
+            // Customize the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = gMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.map_style));
+
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e(TAG, "Can't find style. Error: ", e);
+        }
+
+        // Set the initial camera position
+        LatLng initialPosition = new LatLng(14.5647, 120.99313);
+        float zoomLevel = 19f;
+        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(initialPosition, zoomLevel));
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Navigate to LoginActivity
+        super.onBackPressed();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish(); // Optional: Call finish() if you want to close MainActivity
     }
 }
